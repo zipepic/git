c1cd69d6bbe427d3f4c69432888fc2394d3e7c8d
xzz1p <olezhka.kirilov.97@mail.ru>
2023-02-16 16:07:50 +0300

    выполнил

--- src/main/java/org/example/leson1/Home.java
+++ src/main/java/org/example/leson1/Home.java
@@ -1,7 +1,24 @@
 package org.example.leson1;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class Home {
     public static void main(String[] args) {
-        System.out.println("Hi 1");
+        Totalizator c = new Totalizator();
+        String s = "";
+        c.alienate(s);
+    }
+}
+class Totalizator<E>{
+    public void alienate(E o){
+        Class c = o.getClass();
+        String name = c.getName();
+        Field[] fields = c.getDeclaredFields();
+        Method[] methods = c.getDeclaredMethods();
+        System.out.println(name);
+        Arrays.stream(fields).forEach(System.out::println);
+        Arrays.stream(methods).forEach(System.out::println);
     }
 }
